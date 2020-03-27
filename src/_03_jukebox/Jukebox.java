c3430aedcfa1f6c7abb1dfb510906f376edc3018
emma-l-myoosik <60460092+emma-l-myoosik@users.noreply.github.com>
2020-03-26 18:23:33 -0700

    jukebox

--- src/_03_jukebox/Jukebox.java
+++ src/_03_jukebox/Jukebox.java
@@ -5,6 +5,9 @@ package _03_jukebox;
  */
 
 
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -12,7 +15,10 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import javazoom.jl.player.advanced.AdvancedPlayer;
@@ -20,12 +26,21 @@ import javazoom.jl.player.advanced.AdvancedPlayer;
 /*   If you don't have javazoom.jar in your project, you can download it from here: http://bit.ly/javazoom
  *   Right click your project and add it as a JAR (Under Java Build Path > Libraries).*/
 
-public class Jukebox implements Runnable {
-
+public class Jukebox implements Runnable, MouseListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
+	Song song1;
+	JLabel displayCover1;
+	Song song2;
+	JLabel displayCover2;
+	
     public void run() {
 
 		// 1. Find an mp3 on your computer or on the Internet.
 		// 2. Create a Song object for that mp3
+    	song1 = new Song("BTS (방탄소년단) 'ON' Kinetic Manifesto Film Come Prima.mp3");
+    	song2 = new Song("[MV] BTS(방탄소년단) DOPE(쩔어).mp3");
 
 		// 3. Play the Song
 
@@ -36,7 +51,25 @@ public class Jukebox implements Runnable {
 		 * cover is clicked, stop the currently playing song, and play the one
 		 * that was selected.
 		 */
+    	frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		String albumCover1 = "mapofsoul7.jpg";
+		displayCover1 = loadImage(albumCover1);
+		displayCover1.addMouseListener(this);
+		
+		String albumCover2 = "hyyhpt1.jpg";
+		displayCover2 = loadImage(albumCover2);
+		displayCover2.addMouseListener(this);
+		
+		panel.add(displayCover1);
+		panel.add(displayCover2);
+		frame.add(panel);
+		frame.pack();
+		 
+		
     }
+
     
     
 	/* Use this method to add album covers to your Panel. */
@@ -44,6 +77,54 @@ public class Jukebox implements Runnable {
 		URL imageURL = getClass().getResource(fileName);
 		Icon icon = new ImageIcon(imageURL);
 		return new JLabel(icon);
+	}
+
+
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		JLabel mapofsoul7 = (JLabel) e.getSource();
+		JLabel hyyhpt1 = (JLabel) e.getSource();
+		
+		if (mapofsoul7 == displayCover1) {
+			song1.play();
+		}
+		else if (hyyhpt1 == displayCover2) {
+			song2.play();
+		}
+	}
+
+
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
---
+++ src/_03_jukebox/JukeboxTestor.java
@@ -0,0 +1,8 @@
+package _03_jukebox;
+
+public class JukeboxTestor {
+	public static void mian(String[] args) {
+		Jukebox jukebox = new Jukebox();
+		jukebox.run();
+	}
+}
